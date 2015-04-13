03ea85838349683e71ce204af6dbee19a329f1f5
Milan Rapić <rapic.milan@gmail.com>
2015-04-13 19:42:19 +0200

    Finish with filtering scrolldepth event data, and generate output data

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,12 @@ $(function(){
 
   var _gaq = {};
 
+  var _data = {
+    scroll_depth: "",
+    scroll_velocity: "",
+    time_on_page: ""
+  }
+
   _gaq.push = function(data) {
     console.log("_gaq.push(" + JSON.stringify(data) + ");");
   };
@@ -24,16 +30,92 @@ $(function(){
     elements: ['#section1', '#section2', '#section3', '#section4', '#section5', '#section6', 'footer'],
     userTiming: true,
     eventHandler: function(data) {
-      
+      /*
       console.log("Event: " + data.event);
       console.log("Event action: " + data.eventAction);
       console.log("Event category: " + data.eventCategory);
       console.log("Event label: " + data.eventLabel);
       console.log("Event value: " + data.eventValue);
       console.log("Event timing: " + data.eventTiming);
+      */
+
       
-      console.log(data);
+      tmpScrollDepthPercentage = getPixelDepthPercentage(data);
+      if (tmpScrollDepthPercentage != undefined) {
+        _data.scroll_depth = tmpScrollDepthPercentage;
+      }
+
+      scrollDepth = getPixelDepth(data);
+
+      tmpTimeOnPage = getTimeOnPage(data);
+      if (tmpTimeOnPage != undefined) {
+        _data.time_on_page = tmpTimeOnPage;
+      }
+
+     
+      if (((scrollDepth != undefined) && (_data.scroll_depth != "")) && (_data.time_on_page != undefined)) {
+        var _pixelDepth = parseInt(scrollDepth);
+        
+        _data.scroll_velocity = getPixelVelocity(_pixelDepth, _data.time_on_page)
+
+        if (_data.scroll_velocity == "Infinity") {
+          _data.scroll_velocity = NaN
+        }
+
+        console.log(_data)
+        console.log("Scroll velocity formula:  velocity = pixelDepth / timeOnPage | " + _data.scroll_velocity + " = " + _pixelDepth + " / " + _data.time_on_page)
+
+        console.log("----------------------------------------------------------------------------------");
+
+        _data.scroll_depth = "";
+        _data.scroll_velocity = "";
+        _data.time_on_page = "";
+      }
+      
+      //console.log(_data)
+
+      /*
+        “data”: {
+          “scroll_depth”: %
+          “scroll_velocity”: px/s
+          “time_on_page”: seconds
+        }
+      */
+
+
+
+      //console.log(data);
     }
   });
+
+  function getPixelDepth(data) {
+    if ((data.event == "ScrollDistance") && (data.eventAction == "Pixel Depth")) {
+      var pixelDepth = data.eventLabel;
+
+      return pixelDepth;
+    }
+  }
+
+  function getPixelDepthPercentage(data) {
+    if ((data.event == "ScrollDistance") && (data.eventAction == "Percentage")) {
+      var pixelDepth = data.eventLabel;
+
+      return pixelDepth;
+    }
+  }
+
+  function getPixelVelocity(pixelDepth, timing) {
+      var pixelVelocity = pixelDepth / timing;
+
+      return parseFloat(pixelVelocity.toFixed(2));
+  }
+
+  function getTimeOnPage(data) {
+    if (data.event == "ScrollTiming") {
+      var timeOnPage = data.eventTiming * 0.001
+
+      return timeOnPage;
+    }
+  }
 
 });
